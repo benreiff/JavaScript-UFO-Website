5a58ca02d6ec33680d4029796d97243fb23b8741
Benjamin Reiff <benjamin.reiff@gmail.com>
2020-03-14 23:21:34 -0400

    Third commit with changes to part 2

--- UFO-level-2/app.js
+++ UFO-level-2/app.js
@@ -1,5 +1,7 @@
-// Create a variable from data.js
+// from data.js
 var tableData = data;
+
+// YOUR CODE HERE!
 
 // Create "tbody" variable from tbody HTML element
 var tbody = d3.select("tbody");
@@ -11,13 +13,12 @@ data.forEach(function(sighting) {
   var row = tbody.append("tr");
 
   Object.entries(sighting).forEach(function([key, value]) {
-    // Append a cell to the row for each value
-    // in the weather report object
+
+    // Append a cell to the row for each value in the weather report object
     var cell = row.append("td");
     cell.text(value);
   });
 });
-
 
 
 // Select the button
@@ -25,25 +26,31 @@ var button = d3.select("#filter-btn");
 
 button.on("click", function() {
 
-    var filterClasses = ["date", "city", "state", "country", "shape"];    
-
-
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#date");
+  var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
-  var filteredData = people.filter(person => person.bloodType === inputValue);
+  var filteredDate = tableData.filter(sighting => sighting.datetime === inputValue);
 
-  console.log(filteredData);
+  console.log(filteredDate);
 
+  // Create list variable from tbody HTML element
+  var list = d3.select("tbody");
+
+  // Remove original data from table
+  list.html("");
+
+  // Append the number of table rows for updated data
+  filteredDate.forEach((filteredDate) => {
+    var row = tbody.append("tr");
+
+    Object.entries(filteredDate).forEach(([key, value]) => {
+
+      // Append a cell to the row for each updated value
+      var cell = row.append("td");
+      cell.text(value);
+  });
+  });
 });
-
-
-
-// Create references for inputs based on "data"
-//     var inputElement = d3.selectAll(".list-);
-
-
-// // button.on("click", function() {
